bdbc37c3a9d8f8fba4efdb9ddc9d76e579fca67e
sreedhar1993 <sreedharreddy.arolla@mavs.uta.edu>
2016-03-05 18:40:12 -0600

    Demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ApplicationController
 
   # GET /welcome
-  def index
 
+ def index
+
+ end
+  def parsefile
+    render :file => "#{Rails.root}/app/assets/javascripts/example.json",
+           :content_type => 'application/json',
+           :layout => false
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/parsefile'
+
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
